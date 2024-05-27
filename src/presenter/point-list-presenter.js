c9dbe8d8d60ce50c2657577de6f609ad0c28eeca
MatNepo <matnepo@gmail.com>
2024-05-27 19:29:12 +0300

    msg

--- src/presenter/point-list-presenter.js
+++ src/presenter/point-list-presenter.js
@@ -2,7 +2,9 @@ import TripListView from '../view/trip-list-view';
 import TripPointView from '../view/trip-point-view';
 // import TripPointCreatorView from '../view/trip-point-creator-view';
 import TripPointEditorView from '../view/trip-point-editor-view';
+import SortView from '../view/sort-view.js';
 import { render } from '../render';
+import EmptyListView from '../view/trip-list-empty-view';
 
 export default class PointListPresenter {
   #pointListComponent = new TripListView();
@@ -10,16 +12,27 @@ export default class PointListPresenter {
   #pointsModel = null;
   #pointListPoints = [];
 
-  init(pointListContainer, pointsModel) {
+  constructor(pointListContainer, pointsModel){
     this.#pointListContainer = pointListContainer;
     this.#pointsModel = pointsModel;
-    this.#pointListPoints = [...this.#pointsModel.points];
+  }
 
+  init = () => {
+    this.#pointListPoints = [...this.#pointsModel.points];
+    this.#renderTripList();
+  };
+
+  #renderTripList = () => {
+    if (this.#pointListPoints.length === 0) {
+      render(new EmptyListView(), this.#pointListContainer);
+      return;
+    }
+    render(new SortView(), this.#pointListContainer);
     render(this.#pointListComponent, this.#pointListContainer);
     this.#pointListPoints.forEach((point) => {
       this.#renderPoint(point);
     });
-  }
+  };
 
   #renderPoint = (point) => {
     const tripPointComponent = new TripPointView(point);
